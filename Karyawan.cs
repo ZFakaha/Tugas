a05e059694715bc06b1d21aab4d6f050fd4d757d
ZFakaha <zulfakaha@gmail.com>
2022-04-19 12:00:31 +0700

    upload responsi

---
+++ Karyawan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Responsi2283
+{
+    class Karyawan
+    {
+        //CONSTRUCTOR
+        public Karyawan()
+        {
+
+        }
+        //OVERLOAD
+        public Karyawan(string nama, string nik, int gaji)
+        {
+            Nama = nama;
+            NIK = nik;
+            Gaji = gaji;
+        }
+        
+        //PROPERTIES
+        public string Nama { get; set; }
+        public string NIK { get; set; }
+        public int Gaji { get; set; }
+
+
+    }
+}
